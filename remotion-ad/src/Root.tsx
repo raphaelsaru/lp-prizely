f0c6abc4ebff142b2fbee7b4566683f7a0caeee3
Saru <raphaas@hotmail.com>
2026-04-03 15:28:19 -0300

    feat: render all meta ad formats (9x16, 3x4, 1x1, 16x9)

--- remotion-ad/src/Root.tsx
+++ remotion-ad/src/Root.tsx
@@ -21,6 +21,22 @@ export const RemotionRoot: React.FC = () => {
         width={1080}
         height={1440}
       />
+      <Composition
+        id="MetaAd-1x1"
+        component={MetaAd}
+        durationInFrames={1350}
+        fps={30}
+        width={1080}
+        height={1080}
+      />
+      <Composition
+        id="MetaAd-16x9"
+        component={MetaAd}
+        durationInFrames={1350}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
     </>
   );
 };
